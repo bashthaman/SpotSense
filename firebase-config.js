dc59acb2b3adbdfae9e6a8070738ef263cd9e6f7
bashthaman <sebbiemireles@gmail.com>
2026-07-26 00:01:46 -0500

    Commit firebase config for GitHub Pages deployment

---
+++ firebase-config.js
@@ -0,0 +1,7 @@
+firebase.initializeApp({
+  apiKey: "AIzaSyAHt3KNGyZqSZ5Km6NE-0kK_-atU_SofOM",
+  databaseURL: "https://esp-32db-default-rtdb.firebaseio.com",
+  projectId: "esp-32db",
+});
+
+const SPOTSENSE_KEY = "spot_A1";
